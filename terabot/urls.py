d140b8f5da1dfafddea71776d102cdd61b1ec332
teralumeninfo-ctrl <teralumen.info@gmail.com>
2026-06-12 10:05:39 +0530

    Update urls.py

--- terabot/urls.py
+++ terabot/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
+from django.http import HttpResponse
+
+def health(request):
+    return HttpResponse("OK", status=200)
 
 urlpatterns = [
+    path('health/', health),
     path('api/chatbot/', include('chatbot.urls')),
 ]
