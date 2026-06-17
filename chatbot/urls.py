1a169380aec0c54af3d6caa79f5165f23b9b8743
teralumeninfo-ctrl <teralumen.info@gmail.com>
2026-06-17 14:57:21 +0530

    Update urls.py

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('chat/', views.chat, name='terabot-chat'),
-    path('test-groq/', views.test_groq),
+    path('chat/', views.chat, name='hzhub-chat'),
 ]
